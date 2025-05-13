guillermo-ck/minicalc

Add logging to add and subtract functions

calc.py
@@ -1,7 +1,12 @@
 # calc.py
+import logging
+logging.basicConfig(level=loggin.INFO)
+
 def add(a, b):
+    logging.info(f"Adding {a} + {b}")
     return a + b
 
 def subtract(a, b):
+    logging.info(f"Subtracting {a} - {b}")
     return a - b
 

JungHJ02/git-practice-minicalc

calc.py
@@ -1,7 +1,12 @@
 # calc.py
+import logging
+logging.basicConfig(level=logging.INFO)
+
 def add(a, b):
+    logging.info(f"Adding {a} + {b}")
     return a + b
 
 def subtract(a, b):
+    logging.info(f"Subtracting {a} - {b}")
     return a - b
 